e09565c98163f360e64045a9eb0a894de8a746fa
MelisaDomene <domenemelisa@gmail.com>
2025-07-15 04:29:08 -0300

    se completo el carrousel de imagenes

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+// Esto permite usar Bootstrap desde el script global
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    const carouselElement = document.querySelector('#demo');
+    if (carouselElement) {
+      new bootstrap.Carousel(carouselElement, {
+        interval: 2000,
+        ride: 'carousel'
+      });
+    }
+  }
 }
