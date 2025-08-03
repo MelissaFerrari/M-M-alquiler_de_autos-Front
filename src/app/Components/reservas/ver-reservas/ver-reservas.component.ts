309552003eb4c3fec6dde25f3cc3b8fd765a4f07
MelisaDomene <domenemelisa@gmail.com>
2025-08-03 02:54:39 -0300

    Cuando una reserva es vieja, aparece un mensaje: Reserva vencida

--- src/app/Components/reservas/ver-reservas/ver-reservas.component.ts
+++ src/app/Components/reservas/ver-reservas/ver-reservas.component.ts
@@ -6,19 +6,21 @@ import { BackendService } from 'src/app/Services/backend.service';
   templateUrl: './ver-reservas.component.html',
   styleUrls: ['./ver-reservas.component.css']
 })
-
-export class VerReservasComponent implements OnInit{
+export class VerReservasComponent implements OnInit {
   reservas: any;
-  
-  constructor(private BackendService: BackendService,){
-  }
-  
-  
-  ngOnInit() {
 
+  constructor(private BackendService: BackendService) {}
+
+  ngOnInit() {
     this.BackendService.getreservas().subscribe((data: any) => {
-      console.log("hasta aca llego", data);
-      this.reservas = data;
+      const hoy = new Date();
+
+      // Agregamos campo 'vencida' a cada reserva
+      this.reservas = data.map((reserva: any) => {
+        const fechaFin = new Date(reserva.fechaFin);
+        reserva.vencida = fechaFin < hoy;
+        return reserva;
+      });
     }, error => {
       console.error('Error al obtener reservas:', error);
     });
